291dce0a86d1d0aa65e376b7a6c4a025a37b34dd
dev-imran1 <freealltime247@gmail.com>
2024-05-01 11:53:41 +0600

    project done

--- src/pages/card/Card.jsx
+++ src/pages/card/Card.jsx
@@ -3,10 +3,13 @@ import { PRODUCTS } from '../../produts'
 import { ShopContext } from '../../context/ShopContext'
 import CartItem from './CartItem';
 import './cart.css'
+import { useNavigate } from 'react-router-dom';
+
 
 const Card = () => {
   const {cart, getTotalCartAmount } = useContext(ShopContext);
-  let totalAmount = getTotalCartAmount()
+  let totalAmount = getTotalCartAmount();
+  const navigate = useNavigate();
 
   return (
     <div className='cart'>
@@ -15,16 +18,22 @@ const Card = () => {
       </div>
       <div className="cartItems">
         {PRODUCTS.map((prouduct)=> {
-          if(cart[prouduct.id] !== 0){
+          
+            if(cart[prouduct.id] !== 0){
             return <CartItem data={prouduct}/>
           }
         })}
       </div>
+      {totalAmount > 0 ?
       <div className="checkout">
         <p> Subtotal : $ {totalAmount}</p>
-        <button> Coutinue Shopping</button>
+        <button onClick={()=>navigate("/shop")}> Coutinue Shopping</button>
         <button> CheckOut</button>
       </div>
+      :
+      <h1>Your Card is Empty</h1>
+      
+    }
     </div>
   )
 }
